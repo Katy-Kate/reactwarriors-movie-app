e0b2e49a849decb36942748a99aede4ec4317224
Kate Turalnikova <turaslnikova@gmail.com>
2018-12-05 00:29:47 +0200

    build

---
+++ build/precache-manifest.aa6e4061f7b332c4dea1495a7894525c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f41db46dc41ba054368",
+    "url": "/static/js/main.8f41db46.chunk.js"
+  },
+  {
+    "revision": "9d31116d1e5945e7ee52",
+    "url": "/static/js/1.9d31116d.chunk.js"
+  },
+  {
+    "revision": "8f41db46dc41ba054368",
+    "url": "/static/css/main.f98fae21.chunk.css"
+  },
+  {
+    "revision": "9d31116d1e5945e7ee52",
+    "url": "/static/css/1.00deb0ad.chunk.css"
+  },
+  {
+    "revision": "35ce24f0e24f489f6ec122b40eb34f2e",
+    "url": "/index.html"
+  }
+];
